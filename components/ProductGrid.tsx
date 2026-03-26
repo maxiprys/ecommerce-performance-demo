36d7bb9258c816f4a3a8989469bdf106373c58a0
Maxi Prystupczuk <maximiliano.prystupczuk@miracommerce.com>
2026-03-26 11:27:56 -0300

    feat: removing hook and add format price util

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,9 +1,7 @@
-import { useProducts } from "@/hooks/useProducts";
+import { products } from "@/features/products/data";
 import ProductCard from "@/features/products/components/ProductCard";
 
 export default function ProductGrid() {
-  const products = useProducts();
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       {products.map((product) => (
