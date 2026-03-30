df9d9a59b4c9ff4fd7fd3fd0538cf075075151fc
Maxi Prystupczuk <maximiliano.prystupczuk@miracommerce.com>
2026-03-29 23:01:27 -0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { CartProvider } from "@/features/cart/CartContext";
 import { ProductsProvider } from "@/features/products/ProductsContext";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata = {
   title: "E-commerce Demo",
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <ProductsProvider>
           <CartProvider>
