cf1f9796478e0fd3c7248938bcfc448ecd7942ec
maxiprys <maaxipr@hotmail.com>
2026-04-06 17:10:05 -0300

    fix: fix images domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["fakestoreapi.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "fakestoreapi.com", pathname: "/**" },
+      { protocol: "https", hostname: "api.escuelajs.co", pathname: "/**" },
+      { protocol: "https", hostname: "i.imgur.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "placeimg.com", pathname: "/**" },
+      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
+      { protocol: "https", hostname: "cdsassets.apple.com", pathname: "/**" },
+      { protocol: "https", hostname: "vive-y-se-feliz.com", pathname: "/**" },
+    ],
   },
 };
 
